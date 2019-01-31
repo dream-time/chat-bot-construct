184f0263b7e2b545c5c505e809d15a21b3450498
Unknown <tvaliaschvili1234@gmail.com>
2019-01-31 22:33:59 +0300

    npm init & install

---
+++ conf.js
@@ -0,0 +1,2 @@
+exports.mongouri = 'mongodb://heroku_m9dkxksk:gl620t3f28fenqrjldeofch5me@ds245971.mlab.com:45971/heroku_m9dkxksk'
+exports.port = process.env.port || 8080
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    body_parser = require('body-parser')
+    bcrypt = require('bcrypt')
+    db = require('./db')
+    conf = require('./conf')
+
+var app = express()
+
+db.connect(conf.mongouri, 'heroku_m9dkxksk', (err) => {
+    if(err) {
+        return console.log(err)
+    }
+    app.listen(conf.port)
+})
+
+app.use(body_parser.json())
+
+app.post('/test', (req, res) => {
+    res.send(req.body)
+})
+
+
